ba20452775929cf03fd8626bcb5de3fae4daec35
Lanthanumm <yelisei.novak57@gmail.com>
2024-05-07 19:31:47 +0200

    зробив спрінт 2.0

--- Scripts/Movement.cs
+++ Scripts/Movement.cs
@@ -5,6 +5,7 @@ public partial class Movement : CharacterBody3D
 {
 	[ExportCategory("Movement")]
 	[Export] public float speed;
+	[Export] public float sprintingSpeed;
 	[Export] public float jumpVelocity;
 	[Export] public float gravity;
 	[Export] public float acceleration;
@@ -12,13 +13,18 @@ public partial class Movement : CharacterBody3D
 
 	[ExportCategory("Player parts")]
 	[Export] public Node3D head;
+	[Export] public Camera3D camera;
 
 	[ExportCategory("Settings")]
 	[Export] public float mouseSensivity;
+	[Export] public float fov;	
+
+	private float currentSpeed;
 
     public override void _Ready()
     {
         Input.MouseMode = Input.MouseModeEnum.Captured;
+		currentSpeed = speed;
     }
 
     public override void _Input(InputEvent @event)
@@ -31,6 +37,10 @@ public partial class Movement : CharacterBody3D
 			Vector3 newRotation = head.Rotation;
 			newRotation.X = Mathf.Clamp(head.Rotation.X, Mathf.DegToRad(-89), Mathf.DegToRad(89));
 			head.Rotation = newRotation;
+		}
+		if (@event.IsActionPressed("sprint"))
+		{
+			currentSpeed = sprintingSpeed;
 		}
     }
 
@@ -48,13 +58,16 @@ public partial class Movement : CharacterBody3D
 		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
 		if (direction != Vector3.Zero)
 		{
-			velocity.X = Mathf.MoveToward(Velocity.X, direction.X * speed, acceleration);
-			velocity.Z = Mathf.MoveToward(Velocity.Z, direction.Z * speed, acceleration);
+			velocity.X = Mathf.MoveToward(Velocity.X, direction.X * currentSpeed, acceleration);
+			velocity.Z = Mathf.MoveToward(Velocity.Z, direction.Z * currentSpeed, acceleration);
+			if (currentSpeed == sprintingSpeed && camera.Fov < fov + 10) camera.Fov = Mathf.MoveToward(camera.Fov, fov + 10, 1);
 		}
 		else
 		{
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, decceleration);
 			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, decceleration);
+			currentSpeed = speed;
+			if (camera.Fov > fov) camera.Fov = Mathf.MoveToward(camera.Fov, fov - 10, 1);
 		}
 
 		Velocity = velocity;
